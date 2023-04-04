1282bbf50e1a322695fea756b0d94cfa82bda3b3
MPacris <mpacris@yahoo.com>
2023-04-04 16:15:34 -0500

    updating player, human player and game file

--- player.py
+++ player.py
@@ -1,21 +1,16 @@
 
-
 class Player:
     def __init__(self) -> None:
-        self.player_one = ()
-        self.player_two = ()
+       self.player_one_gesture = ""
+       self.player_two_gesture = ''
+
     
-    def choosing_the_players(self):
-        self.number_of_players = input(f'choose the number of players from 0-2')
-        if self.number_of_players == '0':
-            self.player_one = ai_player_random 
-            self.player_two = ai_player_random
-        elif self.number_of_players == "1":
-            self.player_one = human_player
-            self.player_two = ai_player_random
-        elif self.number_of_players == "2":
-            self.player_one = human_player
-            self.player_two = human_player
+
+    def get_choices (self):
+        self.human.gesture
+    
+
     
     
+
 
