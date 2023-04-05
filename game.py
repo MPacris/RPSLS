3f7b19e7f1c5cedb509abd7532f4801d50ee0642
MPacris <mpacris@yahoo.com>
2023-04-05 13:54:00 -0500

    figured out AI vs Human selecting for players

--- game.py
+++ game.py
@@ -11,12 +11,25 @@ class Game:
     def choosing_the_players(self):
         self.number_of_players = input(f'choose the number of players from 0-2')
         if self.number_of_players == '0':
-            self.player_one = Ai_player()      
+            self.player_one = Ai_player()
+            self.player_two = Ai_player()  
+  
+        elif self.number_of_players == '1':
+            self.player_one = Human()
+            self.player_two = Ai_player() 
+
+        elif self.number_of_players == '2':
+            self.player_one = Human()
+            self.player_two = Human()       
 
 
     def collecting_gestures(self):
         self.player_one_gesture = self.player_one.choosing_gesture()
+        self.player_two_gesture = self.player_two.choosing_gesture()
+        
+
         print(self.player_one_gesture)
+        print(self.player_two_gesture)
         
         
 
